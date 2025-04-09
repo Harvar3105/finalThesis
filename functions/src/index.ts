81ee9120c7990ea4935e925d08ba4cd3c8bfb6ed
Harvar3105 <Petrochenko03@inbox.ru>
2025-04-09 18:25:30 +0300

    functions deployed. Implemented cron for old events

---
+++ functions/src/index.ts
@@ -0,0 +1,50 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+// import {onRequest} from "firebase-functions/v2/https";
+// import * as logger from "firebase-functions/logger";
+
+import * as functions from "firebase-functions/v1";
+import * as admin from "firebase-admin";
+
+admin.initializeApp();
+const db = admin.firestore();
+
+export const expireOldEvents = functions.pubsub
+  .schedule("0 0 * * *")
+  .timeZone("Europe/Tallinn")
+  .onRun(async () => {
+    const now = admin.firestore.Timestamp.now();
+
+    const snapshot = await db
+      .collection("events")
+      .where("end", "<=", now)
+      .where("type", "!=", "canceled")
+      .where("type", "!=", "processed") // Только активные
+      .get();
+
+    if (snapshot.empty) {
+      console.log("No outdated events to process.");
+      return;
+    }
+
+    const batch = db.batch();
+
+    snapshot.docs.forEach((doc) => {
+      batch.update(doc.ref, {
+        // TODO: add logic to set canceled. Set canceled externally?
+        type: "processed",
+      });
+    });
+
+    await batch.commit();
+    console.log(`✅ Updated ${snapshot.size} events to status: processed`);
+  });
+
+
